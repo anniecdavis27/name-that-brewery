329787c9e643a11490c8b49a9abfaf23e63824e8
Annie Davis <anniecdavis27@gmail.com>
2020-05-01 10:11:35 -0400

    second commit

---
+++ JS/beerList.js
@@ -0,0 +1,453 @@
+//beer list variables
+
+let brews = [
+  {
+  id: 1,
+  beer: 'Heady Topper',
+  brewery: 'The Alchemist',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 2,
+  beer: 'Pliny the Elder',
+  brewery: 'Russian River Brewing Company',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 3,
+  beer: 'Julius',
+  brewery: 'Tree House Brewing Company',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 4,
+  beer: 'Dinner',
+  brewery: 'Maine Beer Company',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 5,
+  beer: 'Sip of Sunshine',
+  brewery: 'Lawson\'s Finest Liquids',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 6,
+  beer: 'Society and Solitude',
+  brewery: 'Hill Farmstead',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 7,
+  beer: 'Scaled Way Up',
+  brewery: 'Trillium Brewing Company',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 8,
+  beer: 'Lieutenant Dank',
+  brewery: 'Wicked Weed Brewing',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 9,
+  beer: 'Fundamental Forces',
+  brewery: 'Bottle Logic Brewing',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 10,
+  beer: 'Bourbon County Brand Stout',
+  brewery: 'Goose Island',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 11,
+  beer: 'Pearly Wit',
+  brewery: 'Springdale Beer',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 12,
+  beer: 'Wavelength',
+  brewery: 'Equilibrium',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+  {
+  id: 13,
+  beer: 'Major Tom',
+  brewery: 'Lamplighter',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 14,
+  beer: 'Set West',
+  brewery: 'Coronado Brewing',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 15,
+  beer: 'Rolling In Clouds',
+  brewery: 'Finback',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 16,
+  beer: 'Awoogah IPA',
+  brewery: 'Grimm Artisanal Ales',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 17,
+  beer: 'Witchfinder',
+  brewery: 'Holy Mountain',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 18,
+  beer: 'The Dictator is the People',
+  brewery: 'Threes Brewing Co.',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 19,
+  beer: 'S9',
+  brewery: 'Transmitter',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 20,
+  beer: 'Rec League',
+  brewery: 'Harpoon',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 21,
+  beer: 'Citrus Jammer',
+  brewery: 'Sixpoint Brewery',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 22,
+  beer: 'River Trip',
+  brewery: 'Allagash',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 23,
+  beer: 'Comparison is the Thief of Joy',
+  brewery: 'Resident Culture Brewing Co.',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 24,
+  beer: 'Piece of Future',
+  brewery: 'Eighth State Brewing Co.',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 25,
+  beer: 'Three Times Three Times',
+  brewery: 'Brouwerij West',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 26,
+  beer: 'Champagne Velvet',
+  brewery: 'Upland Brewing Co.',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 27,
+  beer: 'Ultrasphere',
+  brewery: 'Hudson Valley Brewery',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 28,
+  beer: 'Language of Thunder',
+  brewery: 'TrimTab Brewing Co.',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 29,
+  beer: 'So Last Season',
+  brewery: 'Primitive Beer',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 30,
+  beer: 'Wild Pink Robots',
+  brewery: 'Homage Brewing Co.',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 31,
+  beer: 'Triple Mosaic Daydream',
+  brewery: 'Other Half Brewing Co.',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 32,
+  beer: 'Anno Domini MMXIX',
+  brewery: 'Burial Beer Co.',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 33,
+  beer: 'Hidden Bridges',
+  brewery: 'Amalgam Brewing',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 34,
+  beer: 'Sap Haus',
+  brewery: 'Oxbow',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 35,
+  beer: 'Nebo',
+  brewery: 'Fonta Flora',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 36,
+  beer: 'Crescent',
+  brewery: 'Suarez Family Brewery',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 37,
+  beer: 'Joyous Almonds',
+  brewery: 'Moksa',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 38,
+  beer: 'Good \'n Fresh',
+  brewery: 'Alvarado Street,
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 39,
+  beer: 'Adaptive Distortion',
+  brewery: 'Tired Hands',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 40,
+  beer: 'Reclaim the Wild',
+  brewery: 'Burgeon',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 41,
+  beer: 'Palmovka',
+  brewery: 'Schilling',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 42,
+  beer: 'Week 156',
+  brewery: 'Industrial Arts',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 43,
+  beer: 'Peryton',
+  brewery: 'Ogopogo',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 44,
+  beer: 'Inhabited Form',
+  brewery: 'Cerebral Brewing',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 45,
+  beer: 'Meditation on Light',
+  brewery: 'Floodland Brewing',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 46,
+  beer: 'High 5 Lager',
+  brewery: 'Highland Park',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 47,
+  beer: 'Xenia',
+  brewery: 'Fox Farm',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 48,
+  beer: 'Confliction',
+  brewery: 'Sloop Brewing Co.',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 49,
+  beer: 'Barn Beer',
+  brewery: 'Plan Bee Farm Brewery,
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+  {
+  id: 50,
+  beer: 'B-Bomb',
+  brewery: 'Fremont Brewing',
+  image: '',
+  choices: ['a', 'b', 'c', 'd'],
+  answer: '',
+  },
+
+]
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # name-that-brewery
-brewery guessing game
+Calling all craft-beer lovers!
+
+So, you think you know craft beer, huh? Test your knowledge here!
+
+You will be shown a series of 50 cards, each with a different beer can on it showing the
+title of a specific craft brew. You will be given four brewery names and it is your task
+to identify the brewery that makes the beer named in the graphic. To win, you must accurately
+identify 80% of the brews. Good Luck!
+
+Build Plan:
+1) Build design - HTML/CSS, see wireframe
+2) Create beer cards
+3) Add functionality
+  - create array of objects with all of the images, choices and answers
+  - display random question, render the image and the options
+  - make choices buttons work, make sure the function works for specific answers
+  - set pop up to determine if answer was correct or incorrect
+  - create "next question" button, only available once an answer is clicked
+  - set score counter update based on correct answers
+  - once all questions are answered render modal that displays either "You Won..." or "Not quite,..."
+  - create modal to display win or lose
